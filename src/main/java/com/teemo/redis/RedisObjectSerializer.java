4010e40a0d6554a669d2d2b08653a9bc52492fd7
tangjingxiang <396985595@qq.com>
2016-11-02 22:15:25 +0800

    加入Redis支持 2016-11-02

--- src/main/java/com/teemo/redis/RedisObjectSerializer.java
+++ src/main/java/com/teemo/redis/RedisObjectSerializer.java
@@ -1,5 +1,8 @@
 package com.teemo.redis;
 
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.core.serializer.support.DeserializingConverter;
+import org.springframework.core.serializer.support.SerializingConverter;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.SerializationException;
 
@@ -7,13 +10,53 @@ import org.springframework.data.redis.serializer.SerializationException;
  * Created by tangjingxiang on 2016/11/2.
  */
 public class RedisObjectSerializer implements RedisSerializer<Object> {
+
+    //序列化
+    private Converter<Object, byte[]> serializer = new SerializingConverter();
+    //反序列化
+    private Converter<byte[], Object> deserializer = new DeserializingConverter();
+
+
+    static final byte[] EMPTY_ARRAY = new byte[0];
+
+    /**
+     * 序列化
+     * @param object
+     * @return
+     * @throws SerializationException
+     */
     @Override
-    public byte[] serialize(Object o) throws SerializationException {
-        return new byte[0];
+    public byte[] serialize(Object object) throws SerializationException {
+        if (object == null) {
+            return EMPTY_ARRAY;
+        }
+        try {
+            return serializer.convert(object);
+        } catch (Exception ex) {
+            return EMPTY_ARRAY;
+        }
     }
 
+    /**
+     * 反序列化
+     * @param bytes
+     * @return
+     * @throws SerializationException
+     */
     @Override
     public Object deserialize(byte[] bytes) throws SerializationException {
-        return null;
+        if (isEmpty(bytes)) {
+            return null;
+        }
+        try {
+            return deserializer.convert(bytes);
+        } catch (Exception ex) {
+            throw new SerializationException("Cannot deserialize", ex);
+        }
+    }
+
+
+    private boolean isEmpty(byte[] data) {
+        return (data == null || data.length == 0);
     }
 }
--- src/test/java/com/teemo/ApplicationTests.java
+++ src/test/java/com/teemo/ApplicationTests.java
@@ -1,7 +1,6 @@
 package com.teemo;
 
 import com.teemo.entity.User;
-import com.teemo.redis.MyRedisTemplate;
 import com.teemo.service.UserService;
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -21,11 +21,13 @@ import java.util.List;
 @SpringBootTest
 public class ApplicationTests {
 
-//	@Autowired
-//	private StringRedisTemplate redisTemplate ;
+	@Autowired
+	private StringRedisTemplate stringRedisTemplate ;
 
 	@Autowired
-	private MyRedisTemplate redisTemplate;
+	private RedisTemplate<String, Object> redisTemplate;
+
+
 
 	@Autowired
 	private UserService userService ;
@@ -36,31 +38,40 @@ public class ApplicationTests {
 
 	@Test
 	public void testRedis(){
-		redisTemplate.set("test_key", "test_value");
-		Assert.assertEquals("test_value",redisTemplate.get("test_key"));
+		stringRedisTemplate.opsForValue().set("test_key", "test_value");
+
+		Assert.assertEquals("test_value",stringRedisTemplate.opsForValue().get("test_key"));
 	}
+
 
 
 	@Test
-	public void testUserRedis(){
-		List<User> users = userService.getUsers();
-		final String key = "users";
-		redisTemplate.execute(new RedisCallback<Boolean>() {
-			@Override
-			public Boolean doInRedis(RedisConnection connection) throws DataAccessException {
-				RedisSerializer<Object> keySerializer = redisTemplate.getKeySerializer();
-				RedisSerializer<Object> valueSerializer = redisTemplate.getValueSerializer();
-				connection.multi();
-				connection.del(keySerializer.serialize(key));
-
-				for (User user : users){
-					connection.rPush(keySerializer.serialize(key),valueSerializer.serialize(user));
-				}
-				connection.close();
-
-				return true;
-			}
-		});
-
+	public void testObjectRedis(){
+		List<User> list = userService.getUsers();
+		redisTemplate.opsForList().rightPushAll("user_list",list);
 	}
+
+
+//	@Test
+//	public void testUserRedis(){
+//		List<User> users = userService.getUsers();
+//		final String key = "users";
+//		redisTemplate.execute(new RedisCallback<Boolean>() {
+//			@Override
+//			public Boolean doInRedis(RedisConnection connection) throws DataAccessException {
+//				RedisSerializer<Object> keySerializer = redisTemplate.getKeySerializer();
+//				RedisSerializer<Object> valueSerializer = redisTemplate.getValueSerializer();
+//				connection.multi();
+//				connection.del(keySerializer.serialize(key));
+//
+//				for (User user : users){
+//					connection.rPush(keySerializer.serialize(key),valueSerializer.serialize(user));
+//				}
+//				connection.close();
+//
+//				return true;
+//			}
+//		});
+//
+//	}
 }
